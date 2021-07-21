469a4435ba59188526aa486b653c4b8b4e90a9a9
bohdanloik <bohdanloik@gmail.com>
2021-07-21 20:31:55 +0300

    - code refactoring -

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -3,6 +3,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import {action} from '@storybook/addon-actions';
 import AppWithRedux from '../AppWithRedux';
+import { Provider } from 'react-redux';
+import { store } from '../state/store';
 
 export default {
   title: 'Example/AppWithRedux',
@@ -11,6 +13,5 @@ export default {
 
 
 export const AppWithReduxExmple = (props: any) => {
-    return(<AppWithRedux />
-    )
+    return (<Provider store={store}> <AppWithRedux /> </Provider>)
 }
