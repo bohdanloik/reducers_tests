45f836f7771c598f3300f6843117f52e2be9d744
bohdanloik <bohdanloik@gmail.com>
2021-07-22 11:38:15 +0300

    - add initialGlobalState to storybook -

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -5,13 +5,15 @@ import {action} from '@storybook/addon-actions';
 import AppWithRedux from '../AppWithRedux';
 import { Provider } from 'react-redux';
 import { store } from '../state/store';
+import { ReduxStoreProviderDecorator } from './decorators/ReduxStoreProviderDecorator';
 
 export default {
   title: 'Example/AppWithRedux',
   component: AppWithRedux,
+  decorators: [ReduxStoreProviderDecorator]
 }
 
 
 export const AppWithReduxExmple = (props: any) => {
-    return (<Provider store={store}> <AppWithRedux /> </Provider>)
+    return <AppWithRedux /> 
 }
