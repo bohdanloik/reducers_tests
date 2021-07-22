ffad468548f539aa5ea75b05d99e409b07e06914
bohdanloik <bohdanloik@gmail.com>
2021-07-22 14:26:53 +0300

    - add snapshot test to components -

--- integration/app.test.js
+++ integration/app.test.js
@@ -8,4 +8,26 @@ describe('addItemForm', () => {
         expect(image).toMatchImageSnapshot();
     });
  });
+
+ describe('EditableSpan', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:6006/iframe.html?id=example-editablespan--editable-span-exmple');
+        const image = await page.screenshot();
+ 
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+ });
+
+ describe('AppWithRedux', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:6006/iframe.html?id=example-appwithredux--app-with-redux-exmple');
+        const image = await page.screenshot();
+ 
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+ });
  
